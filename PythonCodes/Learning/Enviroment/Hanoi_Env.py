f7377929e21dfd2169f7045b366b1b5be0457479
ProjetoOntologia <projetoontologia@regis.local>
2017-04-25 15:00:26 -0300

    Finalizing Enviroment part 2

--- PythonCodes/Learning/Enviroment/Hanoi_Env.py
+++ PythonCodes/Learning/Enviroment/Hanoi_Env.py
@@ -1,60 +1,72 @@
 #move = (OriginPeg, DestinyPeg)
-#pegs = [Peg1[],Peg2[],Peg3[]]
+#state = [Peg1[],Peg2[],Peg3[]]
 import copy
-from itertools import permutations, combinations
+
+import itertools
+
 class Hanoi_Env:
 
-    pegs = list()
-    n = 0
+    def __init__(self,n=3,p=3):
 
-    def __init__(self,state):
-        for i in state:
-            if self.pegs[i]:
-                self.pegs[i] = state[i]
-            else:
-                self.pegs.insert(i,state[i])
-        self.n = len(self.pegs[0]) + len(self.pegs[1]) + len(self.pegs[2])
-        self.actions = list(permutations(range(self.n),2))
+        self.n = n
+        self.actions = list(itertools.permutations(range(self.n), 2))
+
+        initial_state = list(list([x for x in range(0,n)]).sort().reverse())
+
+        for x in range(0, p):
+            initial_state.append(list())
+
+        self.final_state = initial_state.reverse()
+        self.state = initial_state
 
     def allowed_moves(self, state):
         moves = list()
 
         for x in range(len(state)-1):
             for y in range(x,len(state)):
-                if self.pegs[x] > self.pegs[y]:
+                if self.state[x][-1] > self.state[y][-1]:
                     moves.append((y,x))
                 elif x!=y :
                     moves.append((x,y))
         return moves
-
-    def move_allowed(self,move):
-        disc_from = move[0]
-        disc_to = move[1]
-
-        if self.pegs(disc_from):
-            return (self.pegs(disc_to)[-1] > self.pegs(disc_from)[-1]) if self.pegs(disc_from) else True
-        else:
-            return False
+    #
+    # def move_allowed(self,move):
+    #     disc_from = move[0]
+    #     disc_to = move[1]
+    #
+    #     if self.pegs(disc_from):
+    #         return (self.pegs(disc_to)[-1] > self.pegs(disc_from)[-1]) if self.pegs(disc_from) else True
+    #     else:
+    #         return False
 
     def get_state(self):
-        return self.pegs
+        return copy.copy(self.state)
 
     def get_moved_state(self,move,modify=True):
-
+        stateToshow = None
         if self.move_allowed(move):
             if modify:
-                self.pegs(move[1]).append(self.pegs(move[0]).pop())
-                return [x for x in self.pegs]
+                self.state[move[1]].append(self.state[move[0]].pop())
+                stateToshow = copy.copy(self.state)
 
             else:
-                copyPegs = copy.copy(self.pegs)
-                copyPegs.pegs(move[1]).append(copyPegs.pegs(move[0]).pop())
-                return [x for x in copyPegs.pegs]
+                copyState = copy.copy(self.state)
+                copyState[move[1]].append(copyState[move[0]].pop())
+                stateToshow = copyState
 
-        return self.pegs
+        if self.finished(stateToshow):
+            reward = 10000
+        else:
+            reward = -10
+
+        return self.state,reward
         #state = [self.pegs[0],self.pegs[1],self.pegs[2]]
         #state = [x for x in self.pegs]
 
+    def finished(self,state):
+        if state == self.final_state :
+            return True
+        return False
     # def rewardMatrix(self):
     #     actions = list(permutations(range(3),2))
     #     states =
