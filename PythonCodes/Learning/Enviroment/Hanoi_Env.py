2c31d10f7d6d9f25e0a046c37d804f4112fdbed3
victor <victorblsilveira@gmail.com>
2017-04-24 21:55:21 -0300

    Creating functions

--- PythonCodes/Learning/Enviroment/Hanoi_Env.py
+++ PythonCodes/Learning/Enviroment/Hanoi_Env.py
@@ -3,13 +3,26 @@
 
 class Hanoi_Env:
 
+    pegs = list(3)
+    n = 0
+
     def __init__(self,state):
-        #self.
-        #self.
-        pass
+        self.pegs[0] = state[0]
+        self.pegs[1] = state[1]
+        self.pegs[2] = state[2]
+        self.n = len(self.pegs[0]) + len(self.pegs[1]) + len(self.pegs[2])
 
     def allowed_moves(self,state):
         pass
+
+    def move_allowed(self,move):
+        disc_from = move[0]
+        disc_to = move[1]
+
+        if self.pegs(disc_from):
+            return (self.pegs(disc_to)[-1] > self.pegs(disc_from)[-1]) if self.pegs(disc_from) else True
+        else:
+            return False
 
     def discs_on_peg(self,peg):
         pass
