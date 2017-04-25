5df4ccaba4cef1bab182337860196ebde97a445d
ProjetoOntologia <projetoontologia@regis.local>
2017-04-25 09:34:50 -0300

    Finalizing Enviroment part 1

--- PythonCodes/Learning/Q_Learning.py
+++ PythonCodes/Learning/Q_Learning.py
@@ -1,3 +1,8 @@
-class Q_Leaning:
+class Q_Leaning():
+
+    def __init__(self,gamma=0.8,alpha=1.0,N=1000):
+        self.gamma = gamma
+        self.alpha = alpha
+        self.N_episodes = N
 
     pass
