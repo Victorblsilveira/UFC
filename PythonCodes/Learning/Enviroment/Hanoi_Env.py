db3c8515c90ce3f8c97312b37a3afc644e293d90
ProjetoOntologia <projetoontologia@regis.local>
2017-04-25 09:34:50 -0300

    Finalizing Enviroment part 1

--- PythonCodes/Learning/Enviroment/Hanoi_Env.py
+++ PythonCodes/Learning/Enviroment/Hanoi_Env.py
@@ -1,19 +1,31 @@
-
-
-
+#move = (OriginPeg, DestinyPeg)
+#pegs = [Peg1[],Peg2[],Peg3[]]
+import copy
+from itertools import permutations, combinations
 class Hanoi_Env:
 
-    pegs = list(3)
+    pegs = list()
     n = 0
 
     def __init__(self,state):
-        self.pegs[0] = state[0]
-        self.pegs[1] = state[1]
-        self.pegs[2] = state[2]
+        for i in state:
+            if self.pegs[i]:
+                self.pegs[i] = state[i]
+            else:
+                self.pegs.insert(i,state[i])
         self.n = len(self.pegs[0]) + len(self.pegs[1]) + len(self.pegs[2])
+        self.actions = list(permutations(range(self.n),2))
 
-    def allowed_moves(self,state):
-        pass
+    def allowed_moves(self, state):
+        moves = list()
+
+        for x in range(len(state)-1):
+            for y in range(x,len(state)):
+                if self.pegs[x] > self.pegs[y]:
+                    moves.append((y,x))
+                elif x!=y :
+                    moves.append((x,y))
+        return moves
 
     def move_allowed(self,move):
         disc_from = move[0]
@@ -24,10 +36,33 @@ class Hanoi_Env:
         else:
             return False
 
-    def discs_on_peg(self,peg):
-        pass
+    def get_state(self):
+        return self.pegs
 
-    def get_moved_state(self,move):
-        pass
+    def get_moved_state(self,move,modify=True):
 
+        if self.move_allowed(move):
+            if modify:
+                self.pegs(move[1]).append(self.pegs(move[0]).pop())
+                return [x for x in self.pegs]
 
+            else:
+                copyPegs = copy.copy(self.pegs)
+                copyPegs.pegs(move[1]).append(copyPegs.pegs(move[0]).pop())
+                return [x for x in copyPegs.pegs]
+
+        return self.pegs
+        #state = [self.pegs[0],self.pegs[1],self.pegs[2]]
+        #state = [x for x in self.pegs]
+
+    # def rewardMatrix(self):
+    #     actions = list(permutations(range(3),2))
+    #     states =
+    #     R = list()
+    #     for state in states :
+    #         tower = Hanoi_Env(state)
+    #         for move in actions :
+    #             if tower.move_allowed(move):
+    #                 next_state = tower.get_moved_state()
+    #                 R[state][next_state] = -5
+    #     pass
--- PythonCodes/Learning/Sarsa_Learning.py
+++ PythonCodes/Learning/Sarsa_Learning.py
@@ -1,3 +1,94 @@
+import random
+
+import numpy as np
+
+from Enviroment import Hanoi_Env
+
 class Sarsa_Learning:
 
-    pass
+
+    def __init__(self, gamma=0.8, alpha=1.0, N=10000):
+        self.gamma = gamma
+        self.alpha = alpha
+        self.N_episodes = N
+
+    def computeQ(self):
+        # Initialize the Q matrix 16(rows)x4(columns)
+        Q = np.zeros([env.observation_space.n, env.action_space.n])
+
+        for i in range(env.observation_space.n):
+            if (i != 5) and (i != 7) and (i != 11) and (i != 12) and (i != 15):
+                for j in range(env.action_space.n):
+                    Q[i, j] = np.random.rand()
+
+        # Epsilon-Greedy policy, given a state the agent chooses the action that it believes has the best long-term effect with probability 1-eps, otherwise, it chooses an action uniformly at random. Epsilon may change its value.
+
+        bestreward = 0
+        epsilon = 0.1
+        discount = 0.99
+        learning_rate = 0.1
+        a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+
+        for i_episode in range(self.N_episodes):
+
+            number_of_discs = 3
+            number_of_pegs = 3
+
+            initial_state = list(list([x for x in range(0,number_of_discs)]).sort().reverse())
+
+            for x in range(0,number_of_pegs):
+                initial_state.append(list([0 for y in range(0,number_of_discs)]))
+
+            tower = Hanoi_Env(initial_state)
+
+            currentState = tower.get_state()
+
+            # Select action a using a policy based on Q
+            if np.random.rand() <= epsilon:  # pick randomly
+                currentAction = random.randint(0, env.action_space.n - 1)
+            else:  # pick greedily
+                currentAction = np.argmax(Q[currentState, :])
+
+            totalreward = 0
+            while True:
+                env.render()
+
+                # Carry out an action a
+                observation, reward, done, info = env.step(currentAction)
+                if done is True:
+                    break;
+
+                # Observe reward r and state s'
+                totalreward += reward
+                nextState = observation
+
+                # Select action a' using a policy based on Q
+                if np.random.rand() <= epsilon:  # pick randomly
+                    nextAction = random.randint(0, env.action_space.n - 1)
+                else:  # pick greedily
+                    nextAction = np.argmax(Q[nextState, :])
+
+                # update Q with Q-learning
+                Q[currentState, currentAction] += learning_rate * (
+                reward + discount * Q[nextState, nextAction] - Q[currentState, currentAction])
+
+                currentState = nextState
+                currentAction = nextAction
+
+                print "Episode: %d reward %d best %d epsilon %f" % (i_episode, totalreward, bestreward, epsilon)
+                if totalreward > bestreward:
+                    bestreward = totalreward
+                if i_episode > self.N_episodes / 2:
+                    epsilon = epsilon * 0.9999
+                if i_episode >= self.N_episodes - 10:
+                    a.insert(0, totalreward)
+                    a.pop()
+                print a
+
+                for i in range(env.observation_space.n):
+                    print "-----"
+                    for j in range(env.action_space.n):
+                        print Q[i, j]
+        pass
