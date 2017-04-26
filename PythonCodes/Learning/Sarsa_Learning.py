a86471578bdf9d6513a13e689adbcd45cb8c8557
ProjetoOntologia <projetoontologia@regis.local>
2017-04-26 15:54:17 -0300

    Finalizing Sarsa part 2

--- PythonCodes/Learning/Sarsa_Learning.py
+++ PythonCodes/Learning/Sarsa_Learning.py
@@ -1,7 +1,7 @@
 import random
-
+import copy
 import numpy as np
-
+import operator
 from Enviroment import Hanoi_Env
 
 class Sarsa_Learning:
@@ -15,30 +15,24 @@ class Sarsa_Learning:
     def computeQ(self):
 
         tower = Hanoi_Env.Hanoi_Env(3)
+
         Q = dict()
-        # Initialize the Q matrix 16(rows)x4(columns)
+
         stateIdentifier = str(tower.get_state())
+
         Q[stateIdentifier] = dict()
         for x in tower.actions:
             Q[stateIdentifier][x] = 0
 
-        # for i in range(env.observation_space.n):
-        #     if (i != 5) and (i != 7) and (i != 11) and (i != 12) and (i != 15):
-        #         for j in range(env.action_space.n):
-        #             Q[i, j] = np.random.rand()
-
-        # Epsilon-Greedy policy, given a state the agent chooses the action that it believes has the best long-term effect with probability 1-eps, otherwise, it chooses an action uniformly at random. Epsilon may change its value.
-
-        bestreward = 0
         epsilon = 0.1
         discount = 0.99
         learning_rate = 0.1
-        a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-        #print "Estado inicial de Q : " + str(Q)
 
         for i_episode in range(self.N_episodes):
+
             number_of_moves = 0
+
             currentState = tower.get_state()
 
             # Select action a using a policy based on Q
@@ -46,17 +40,17 @@ class Sarsa_Learning:
             if np.random.rand() <= epsilon:  # pick randomly
                 currentAction = random.randint(0, len(moves)-1)
             else:  # pick greedily
-                currentAction = np.argmax(Q[str(currentState)])
+                currentAction = np.argmax(Q[str(currentState)].values())
 
-            #print "Primeira acao : "+str(moves[currentAction])
             totalreward = 0
+            #print moves
+            while not tower.finished(currentState):
 
-            while not tower.finished(currentState) :
                 number_of_moves = number_of_moves +1
                 # Carry out an action a
+                print currentState
                 nextState, reward = tower.get_moved_state(tower.actions[currentAction])
-
-                #print "Proximo estado :"+str(nextState)
+                print currentState
                 # Observe reward r and state s'
                 totalreward += reward
 
@@ -64,40 +58,29 @@ class Sarsa_Learning:
                 stateIdentifier = str(nextState)
                 if not stateIdentifier in Q.keys():
                     Q[stateIdentifier] = dict()
-                    for x in tower.actions:
+                    for x in moves:
                         Q[stateIdentifier][x] = 0
 
                 # Select action a' using a policy based on Q
-                moves = tower.actions
                 if np.random.rand() <= epsilon:  # pick randomly
                     nextAction = random.randint(0, len(moves)-1)
                 else:  # pick greedily
-                    nextAction = np.argmax(Q[str(nextState)])
+                    nextAction = np.argmax(Q[str(nextState)].values())
 
-                #print Q[str(currentState)]
-                # update Q with Q-learning
-                #print ("Proxima acao :"+str(nextAction))
-                #print(nextState)
-                #print Q[str(nextState)][tower.actions[nextAction]]
-                #print Q[str(currentState)][tower.actions[currentAction]]
+
                 Q[str(currentState)][tower.actions[currentAction]] += learning_rate * (
                     reward + discount * Q[str(nextState)][tower.actions[nextAction]] - Q[str(currentState)][tower.actions[currentAction]])
 
-                currentState = nextState
-                currentAction = nextAction
-                # print ("Episode: %d reward %d best %d epsilon %f" % (i_episode, totalreward, bestreward, epsilon))
-                # if totalreward > bestreward:
-                #     bestreward = totalreward
-                # if i_episode > self.N_episodes / 2:
-                #     epsilon = epsilon * 0.9999
-                # if i_episode >= self.N_episodes - 10:
-                #     a.insert(0, totalreward)
-                #     a.pop()
-                # print (a)
+                # print(currentState)
+                # print(moves[currentAction])
+                # print(nextState)
+                # print(moves[nextAction])
+                # print Q
 
-                # for i in range(env.observation_space.n):
-                #     print "-----"
-                #     for j in range(len(tower.actions)):
-                #         print Q[i, j]
+                currentState = copy.copy(nextState)
+                currentAction = copy.copy(nextAction)
+                if number_of_moves > 4:
+                    break;
             tower.restart()
+            break;
             print number_of_moves
