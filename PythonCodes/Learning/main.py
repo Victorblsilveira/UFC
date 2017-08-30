b1a27f85a2e9b15f4b22a0fb9c24832e036945e8
victor <victorblsilveira@gmail.com>
2017-08-30 01:07:03 -0300

    FInalized

--- PythonCodes/Learning/main.py
+++ PythonCodes/Learning/main.py
@@ -1,7 +1,7 @@
 import SarsaLearning as Learn
 from Enviroment import HanoiEnv as Env
 
-learn = Learn.SarsaLearning(Env.HanoiEnv(8))
+learn = Learn.SarsaLearning(Env.HanoiEnv(3))
 
 learn.compute_q()
 
