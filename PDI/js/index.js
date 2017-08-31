8a38a02c24788172a6e5a74669c6833c49d5a00f
victor <victorblsilveira@gmail.com>
2017-08-31 00:03:04 -0300

    Iniciando estilos e botões

---
+++ PDI/js/index.js
@@ -0,0 +1,6 @@
+function black_White(){console.log('preto branco');}
+function potenc(){console.log('potencia');}
+function negative(){console.log('negativo');}
+function potenc_inv(){console.log('raiz quadrada');}
+function log(){console.log('log');}
+function subtract(){console.log('subtract');}
